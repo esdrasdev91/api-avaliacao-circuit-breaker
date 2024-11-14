8983ad20f7ebd3dde7c83ec64ab180db53057f7f
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-14 11:25:08 -0300

    First commit

---
+++ src/main/java/com/esdrasdev/api_avaliacao_circuit_breaker/ApiAvaliacaoCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.api_avaliacao_circuit_breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAvaliacaoCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAvaliacaoCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esdrasdev/api_avaliacao_circuit_breaker/ApiAvaliacaoCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.api_avaliacao_circuit_breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAvaliacaoCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
